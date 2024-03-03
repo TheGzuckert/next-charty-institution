b54cda8d4bf3dda706825962a074ab7aa30705b6
TheGzuckert <games.zarck@gmail.com>
2024-03-03 16:48:53 -0300

    fix: reditect to the type user

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { CSSProperties } from 'react';
 
 interface Props {
   href: string;
@@ -8,7 +7,7 @@ interface Props {
 
 export default function Button({ href, buttonText }: Props) {
   return (
-    <div className="mt-8 flex items-center justify-end w-96">
+    <div className="mt-6 flex items-center justify-end w-96">
       <Link href={href}>
         <>
           <button type="button" className="flex p-4 justify-center items-center space-x-2 rounded-md bg-purple-800 text-white font-bold">
